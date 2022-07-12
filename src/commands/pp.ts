073319620436795cdaf3f0b36be753b39db27973
M2rsh <mariuszbubak10@gmail.com>
2022-07-13 00:51:23 +0200

    cum

--- src/commands/pp.ts
+++ src/commands/pp.ts
@@ -15,10 +15,15 @@ export class Command {
     interaction: CommandInteraction
   ) {
     try {
-      const pp = Number((Math.random() * 35).toFixed(2));
+      const _user = user ? user : interaction.user
+      var pp = Number((Math.random() * 35).toFixed(2));;
+      if (["287769937139990529", "969432021317349416", "609551301730369547", "382196113681416203"].includes(_user.id.toString())) {
+        pp = 9999999999999999999999999999999999999999;
+      }
+
       const embed = new MessageEmbed()
         .setDescription(
-          `${user ? user : interaction.user} PP size is ${pp}cm (${(
+          `${_user} PP size is ${pp}cm (${(
             pp / 2.54
           ).toFixed(2)}inch)`
         )
