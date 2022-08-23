0ef4ee032b3e7f874bb0349c7d046c842aadcfda
M2rsh <mariuszbubak10@gmail.com>
2022-08-23 02:12:46 +0200

    balls

--- src/commands/stfu.ts
+++ src/commands/stfu.ts
@@ -1,16 +1,26 @@
-import { CommandInteraction, PermissionsBitField } from "discord.js";
-import { Discord, Guild, Slash } from "discordx";
+import { ApplicationCommandOptionType, CommandInteraction, PermissionsBitField, User } from "discord.js";
+import { Discord, Slash, SlashChoice, SlashOption } from "discordx";
 import { ErrorHandler } from "../utils/error_handler.js";
 
 @Discord()
 export class Command {
   @Slash({name: "stfu", description: "Shut the fuck up boyetrash", dmPermission: false, defaultMemberPermissions: PermissionsBitField.Flags.UseApplicationCommands })
   async command(
+    @SlashChoice({ name: "Boyetrash", value: "<@560205926758612994>" })
+    @SlashChoice({ name: "Coderunner", value: "<@934178009664069632>" })
+    @SlashOption({
+      name: "user",
+      description: "Common users who should shut the fuck up",
+      required: false,
+      type: ApplicationCommandOptionType.String,
+    })
+    user: string | undefined,
     interaction: CommandInteraction
   ): Promise<void> {
     try {
+
       await interaction.deferReply({ ephemeral: true });
-      interaction.channel?.send({ content: "Shut the fuck up <@560205926758612994>" });
+      interaction.channel?.send({ content: `Shut the fuck up ${user ? user : "<@560205926758612994>"}` });
     } catch (e) {
       await ErrorHandler(e, interaction);
     }
