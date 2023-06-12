0fa6549ae6db8b1a9615e53a28e236812e05c047
M2rsh <mariuszbubak10@gmail.com>
2023-06-12 22:10:52 +0200

    Removed ban command (it was shit) added robots.txt and funny stuff

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -26,12 +26,51 @@ export class API {
   logs(ctx: Context, next: Next): Promise<Next> {
     const _files = fs.readdirSync("logs/");
     var files: any = [];
-    _files.forEach((file) => {
+    _files.forEach((file: string) => {
       if (file.endsWith(".log")) {
         files.push(+file.replace(".log", ""));
       }
     });
     ctx.body = fs.readFileSync(`logs/${Math.max(...files)}.log`, "utf8");
+    return next()
+  }
+  @Get("/robots.txt")
+  robots(ctx: Context, next: Next): Promise<Next> {
+    ctx.body = `User-agent: Googlebot
+Disallow: /
+User-agent: googlebot-image
+Disallow: /
+User-agent: googlebot-mobile
+Disallow: /
+User-agent: MSNBot
+Disallow: /
+User-agent: Slurp
+Disallow: /
+User-agent: Teoma
+Disallow: /
+User-agent: Gigabot
+Disallow: /
+User-agent: Robozilla
+Disallow: /
+User-agent: Nutch
+Disallow: /
+User-agent: ia_archiver
+Disallow: /
+User-agent: baiduspider
+Disallow: /
+User-agent: naverbot
+Disallow: /
+User-agent: yeti
+Disallow: /
+User-agent: yahoo-mmcrawler
+Disallow: /
+User-agent: psbot
+Disallow: /
+User-agent: yahoo-blogs/v3.9
+Disallow: /
+User-agent: *
+Disallow: /
+Disallow: /`
     return next()
   }
 }
---
+++ src/commands/message.ts
@@ -0,0 +1,61 @@
+import {
+  ApplicationCommandOptionType,
+  CommandInteraction,
+  EmbedBuilder,
+  WebhookClient,
+} from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+import { ErrorHandler } from "../utils/error_handler.js";
+import { botLogger } from "../main.js";
+
+var webhook: any;
+if (process.env.WEBHOOK_URL) {
+  webhook = new WebhookClient({
+    url: process.env.WEBHOOK_URL,
+  });
+  botLogger.log("Info", `Webhook Initialized Successfully`);
+}
+
+@Discord()
+export class Command {
+  @Slash({
+    name: "message",
+    description: "Send a message to a special channel",
+    dmPermission: true,
+  })
+  async command(
+    @SlashOption({
+      name: "message",
+      description: "Message to send",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    message: string,
+    interaction: CommandInteraction
+  ) {
+    try {
+      const embed = new EmbedBuilder()
+        .setDescription(message)
+        .setFooter({ text: interaction.guild?.name || "From DMs" })
+        .setColor("#c4a7e7")
+        .setTimestamp();
+
+      webhook.send({
+        username: interaction.user.username,
+        avatarURL: interaction.user.avatarURL(),
+        embeds: [embed],
+      });
+
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(`Message sent`)
+            .setColor("#c4a7e7")
+            .setTimestamp(),
+        ],
+      });
+    } catch (e) {
+      await ErrorHandler(e, interaction);
+    }
+  }
+}
